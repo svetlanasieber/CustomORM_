e0c067e9068296b428f0ef45cd69134e30596bfb
Svetlana Sieber <135451084+svetlanasieber@users.noreply.github.com>
2025-03-11 02:28:29 +0100

    Update DbContext.java

--- src/main/java/bg/softuni/orm/context/DbContext.java
+++ src/main/java/bg/softuni/orm/context/DbContext.java
@@ -4,11 +4,11 @@ import java.lang.reflect.InvocationTargetException;
 import java.sql.SQLException;
 
 public interface DbContext<E> {
-    boolean persist(E entity) throws IllegalAccessException, SQLException; //  Insert | Update
+    boolean persist(E entity) throws IllegalAccessException, SQLException; 
 
-    Iterable<E> find(Class<E> table) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; // Select
-    Iterable<E> find(Class<E> table, String where) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; // Select ... where...
+    Iterable<E> find(Class<E> table) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; 
+    Iterable<E> find(Class<E> table, String where) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; 
 
-    E findFirst(Class<E> table) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; // Select ... limit 1
-    E findFirst(Class<E> table, String where) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; // Select ... where ... limit 1
+    E findFirst(Class<E> table) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException; 
+    E findFirst(Class<E> table, String where) throws SQLException, InvocationTargetException, NoSuchMethodException, InstantiationException, IllegalAccessException;
 }
